222d96764a73bf1b1f3c911d5b276d739d43987f
User <user@AdmindeMacBook-Air.local>
2023-09-18 14:28:10 +0800

    featg: add download handler

--- handler/handler.go
+++ handler/handler.go
@@ -70,3 +70,25 @@ func GetFileMetaHandler( w http.ResponseWriter, r *http.Request) {
 	 }
 	 w.Write(data)
 }
+
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("content-disposition", "attachment; filename=\""+fm.FileName+"\"")
+	w.Write(data)
+}
