66d0c12b8d6938f6b3a3609ff43e9fbec1a77106
User <user@AdmindeMacBook-Air.local>
2023-09-18 14:56:14 +0800

    feat: add table.sql

---
+++ doc/table.sql
@@ -0,0 +1,55 @@
+CREATE TABLE `tbl_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_sha1` char(40) NOT NULL DEFAULT '' COMMENT '文件hash',
+  `file_name` varchar(256) NOT NULL DEFAULT '' COMMENT '文件名',
+  `file_size` bigint(20) DEFAULT '0' COMMENT '文件大小',
+  `file_addr` varchar(1024) NOT NULL DEFAULT '' COMMENT '文件存儲存置',
+  `create_at` datetime default NOW() COMMENT '創建日期',
+  `update_at` datetime default NOW() on update current_timestamp() COMMENT '更新日期',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '狀態(可用/禁用/已删除等狀態)',
+  `ext1` int(11) DEFAULT '0' COMMENT '備用字段1',
+  `ext2` text COMMENT '備用字段2',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_file_hash` (`file_sha1`),
+  KEY `idx_status` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL DEFAULT '' COMMENT '用户名',
+  `user_pwd` varchar(256) NOT NULL DEFAULT '' COMMENT '用户encoded密码',
+  `email` varchar(64) DEFAULT '' COMMENT '郵箱',
+  `phone` varchar(128) DEFAULT '' COMMENT '手機號',
+  `email_validated` tinyint(1) DEFAULT 0 COMMENT '郵箱是否已驗證',
+  `phone_validated` tinyint(1) DEFAULT 0 COMMENT '手機號是否已驗證',
+  `signup_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '註冊日期',
+  `last_active` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后活跃时间戳',
+  `profile` text COMMENT '用户属性',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '賬戶狀態(啟用/禁用/鎖定/標記刪除等)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`user_name`),
+  KEY `idx_status` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tbl_user_token` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL DEFAULT '' COMMENT '用户名',
+  `user_token` char(40) NOT NULL DEFAULT '' COMMENT '用戶登錄token',
+    PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tbl_user_file` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL,
+  `file_sha1` varchar(64) NOT NULL DEFAULT '' COMMENT '文件hash',
+  `file_size` bigint(20) DEFAULT '0' COMMENT '文件大小',
+  `file_name` varchar(256) NOT NULL DEFAULT '' COMMENT '文件名',
+  `upload_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '上傳時間',
+  `last_update` datetime DEFAULT CURRENT_TIMESTAMP
+          ON UPDATE CURRENT_TIMESTAMP COMMENT '最後修改時間',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '文件狀態(0正常1已刪除2禁用)',
+  UNIQUE KEY `idx_user_file` (`user_name`, `file_sha1`),
+  KEY `idx_status` (`status`),
+  KEY `idx_user_id` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
