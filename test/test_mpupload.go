ee1fe746009914181250f9d7ecb8133f12c1fe79
User <user@AdmindeMacBook-Air.local>
2023-09-21 15:00:18 +0800

    feat: add script to test multipart upload

---
+++ test/test_mpupload.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+
+	jsonit "github.com/json-iterator/go"
+)
+
+func multipartUpload(filename string, targetURL string, chunkSize int) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer f.Close()
+
+	bfRd := bufio.NewReader(f)
+	index := 0
+
+	ch := make(chan int)
+	buf := make([]byte, chunkSize)
+	for {
+		n, err := bfRd.Read(buf)
+		if n <= 0 {
+			break
+		}
+		index++
+
+		bufCopied := make([]byte, 5*1048576)
+		copy(bufCopied, buf)
+
+		go func(b []byte, curIdx int) {
+			fmt.Printf("upload_size: %d\n", len(b))
+
+			resp, err := http.Post(
+				targetURL+"&index="+strconv.Itoa(curIdx),
+				"multipart/form-data",
+				bytes.NewReader(b))
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			body, er := ioutil.ReadAll(resp.Body)
+			fmt.Printf("%+v %+v\n", string(body), er)
+			resp.Body.Close()
+
+			ch <- curIdx
+		}(bufCopied[:n], index)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err.Error())
+			}
+		}
+	}
+
+	for idx := 0; idx < index; idx++ {
+		select {
+		case res := <-ch:
+			fmt.Println(res)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	username := "admin"
+	token := "54eefa7dbd5bcf852c52fecd816f2a315c61832c"
+	filehash := "dfa39cac093a7a9c94d25130671ec474d51a2995"
+
+	resp, err := http.PostForm(
+		"http://localhost:8089/file/mpupload/init",
+		url.Values{
+			"username": {username},
+			"token":    {token},
+			"filehash": {filehash},
+			"filesize": {"132489256"},
+		})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	uploadID := jsonit.Get(body, "data").Get("UploadID").ToString()
+	chunkSize := jsonit.Get(body, "data").Get("ChunkSize").ToInt()
+	fmt.Printf("uploadid: %s  chunksize: %d\n", uploadID, chunkSize)
+
+	filename := "E:\\elastic\\elasticsearch-7.1.0-windows-x86_64.zip"
+	tURL := "http://localhost:8089/file/mpupload/uppart?" +
+		"username=admin&token=" + token + "&uploadid=" + uploadID
+	multipartUpload(filename, tURL, chunkSize)
+
+	resp, err = http.PostForm(
+		"http://localhost:8089/file/mpupload/complete",
+		url.Values{
+			"username": {username},
+			"token":    {token},
+			"filehash": {filehash},
+			"filesize": {"132489256"},
+			"filename": {"elasticsearch-7.1.0-windows-x86_64.zip"},
+			"uploadid": {uploadID},
+		})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+	fmt.Printf("complete result: %s\n", string(body))
+}
